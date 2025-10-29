9abb48fd95a334a556dfaa2f234f5a362cf72d00
Andrés Eduardo Rojas Jiménez <andreserojas@uned.cr>
2025-10-28 19:58:49 -0700

    Add project files.

---
+++ Proyecto3.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto3.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
