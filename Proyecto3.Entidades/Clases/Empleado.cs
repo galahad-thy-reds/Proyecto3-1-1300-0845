d1db4480f2cbeabcb9b5c54c8448fd463a9b65e9
Andrés Eduardo Rojas Jiménez <andreserojas@uned.cr>
2025-10-29 21:47:55 -0700

    Se agrega la Entidad Empleado. Se agregan DataAnnotation para la llave primaria y algunos tipos de datos (como Date). Se le modifica el tipo de dato a SalarioDiario de double a decimal para compatibilida con la base de datos

---
+++ Proyecto3.Entidades/Clases/Empleado.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto3.Entidades.Clases
+{
+    public class Empleado
+    {
+        #region Propiedades
+        /// <summary>
+        /// Cedula del Empleado
+        /// </summary>
+        [Key]
+        [Required(ErrorMessage = "Por favor, digite la cedula del empleado")]
+        public string? Cedula { get; set; }
+        /// <summary>
+        /// Fecha de nacimiento del Empleado
+        /// </summary>
+        [DataType(DataType.Date)]
+        [DisplayName("Fecha Nacimiento")]
+        [Required(ErrorMessage = "Por favor, digite la fecha de nacimiento")]
+        public DateOnly FechaNacimiento { get; set; }
+        /// <summary>
+        /// Fecha de ingreso a la empresa del Empleado
+        /// </summary>
+        [DataType(DataType.Date)]
+        [DisplayName("Fecha de Ingreso")]
+        [Required(ErrorMessage = "Por favor, digite la fecha de ingreso")]
+        public DateOnly FechaIngreso { get; set; }
+        /// <summary>
+        /// Salario por dia del Empleado
+        /// </summary>
+        [DisplayName("Salario por Dia")]
+        [Required(ErrorMessage = "Por favor, el salario por dia")]
+        public decimal SalarioDiario { get; set; }
+        /// <summary>
+        /// Fecha de retiro del Empleado, si no se ha retirado es null
+        /// </summary>
+        [DataType(DataType.Date)]
+        [DisplayName("Fecha de Retiro")]
+        public DateOnly? FechaRetiro { get; set; }
+        /// <summary>
+        /// Tipo de contrato del Empleado, ejemplos: veterinario, asistente, administrativo, mantenimiento, groomer.
+        /// </summary>
+        [DisplayName("Tipo de Empleado")]
+        [Required(ErrorMessage = "Por favor, seleccione un tipo de empleado")]
+        public string? TipoEmpleado { get; set; }
+        #endregion
+    }
+}
