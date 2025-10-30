29ea8baeb8783c977d887fff79b7343491f8c41a
Andrés Eduardo Rojas Jiménez <andreserojas@uned.cr>
2025-10-29 21:46:11 -0700

    Se agregan las dependencies a paquetes EntityFrameworkCore.SqlServer, EntityFrameworkCore.Tools. Se agrega la clase DBContexto que hereda del DBContext para EntityFramework

---
+++ Proyecto3.AccesoDatos/AccesoDB/DBContexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto3.Entidades.Clases;
+
+namespace Proyecto3.AccesoDatos.AccesoDB
+{
+    public class DBContexto(DbContextOptions<DBContexto> options) : DbContext(options)
+    {
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
