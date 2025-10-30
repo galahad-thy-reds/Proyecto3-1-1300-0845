e240ee391662d2b8ba03ef9864cd78bcb78f6c2a
Andrés Eduardo Rojas Jiménez <andreserojas@uned.cr>
2025-10-30 13:47:26 -0700

    Se agrega la descripcion de la clase y el DbSet<Empleado> Empleados. Se agregan los DbSet para Clientes, Mascotas, Procedimiento y TiposProcedimiento

--- Proyecto3.AccesoDatos/AccesoDB/DBContexto.cs
+++ Proyecto3.AccesoDatos/AccesoDB/DBContexto.cs
@@ -3,8 +3,31 @@ using Proyecto3.Entidades.Clases;
 
 namespace Proyecto3.AccesoDatos.AccesoDB
 {
+    /// <summary>
+    /// Clase de contexto de la base de datos
+    /// </summary>
+    /// <param name="options">Objeto DbContextOptions<DBContexto> para la injeccion de dependencias de las opciones</param>
     public class DBContexto(DbContextOptions<DBContexto> options) : DbContext(options)
     {
+        /// <summary>
+        /// DbSet de Empleados
+        /// </summary>
         public DbSet<Empleado> Empleados { get; set; }
+        /// <summary>
+        /// DbSet de Clientes
+        /// </summary>
+        public DbSet<Cliente> Clientes { get; set; }
+        /// <summary>
+        /// DbSet de Mascotas
+        /// </summary>
+        public DbSet<Mascota> Mascotas { get; set; }
+        /// <summary>
+        /// Dbset de Procedimientos
+        /// </summary>
+        public DbSet<Procedimiento> Procedimientos { get; set; }
+        /// <summary>
+        /// Dbset de Tipo Procedimientos
+        /// </summary>
+        public DbSet<TipoProcedimiento> TiposProcedimiento { get; set; }
     }
 }
